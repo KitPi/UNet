75ca02e74b390a5bf768cc1997a4849f8f9511bb
Kit Peer <kit.p1@hotmail.com>
2025-05-23 00:17:16 -0700

    layering

--- main.py
+++ main.py
@@ -14,5 +14,55 @@ class UNet(nn.Module):
     def __init__(self):
         super(UNet, self).__init__()
 
+        # max pool layer
         self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # down convolution layers
+        self.down_convolution_1 = double_convolution(3, 64)
+        self.down_convolution_2 = double_convolution(64, 128)
+        self.down_convolution_3 = double_convolution(128,256)
+        self.down_convolution_4 = double_convolution(256, 512)
+        self.down_convolution_5 = double_convolution(512, 1024)
+
+        # up convolution layers
+        self.up_convolution_1 = double_convolution(1024, 512)
+        self.up_convolution_2 = double_convolution(512,256)
+        self.up_convolution_3 = double_convolution(256,128)
+        self.up_convolution_4 = double_convolution(128,64)
+
+        # up transpose layers
+        self.up_transpose_1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
+        self.up_transpose_2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
+        self.up_transpose_3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+        self.up_transpose_4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+
+        # output layer
+        self.out = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3)
+
+    def forward(self, x):
         
+        # down encoding
+        down_1 = self.down_convolution_1(x)
+        down_2 = self.max_pool2d(down_1)
+        down_3 = self.down_convolution_2(down_2)
+        down_4 = self.max_pool2d(down_3)
+        down_5 = self.down_convolution_3(down_4)
+        down_6 = self.max_pool2d(down_5)
+        down_7 = self.down_convolution_4(down_6)
+        down_8 = self.max_pool2d(down_7)
+        down_9 = self.down_convolution_4(down_8)
+
+        # up decoding
+        up_1 = self.up_transpose_1(down_9)
+        x = self.up_convolution_1(torch.cat([down_7, up_1]))
+        up_2 = self.up_transpose_2(x)
+        x = self.up_convolution_2(torch.cat([down_5, up_2]))
+        up_3 = self.up_transpose_3(x)
+        x = self.up_convolution_3(torch.cat([down_3, up_3]))
+        up_4 = self.up_transpose_4(x)
+        x = self.up_convolution_4(torch.cat([down_1, up_4]))
+
+        # output
+        out = self.out(x) 
+
+        return out
